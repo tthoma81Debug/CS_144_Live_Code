7f36a5cdf389f1937980c08904b88f6bc1693143
thomastyl <tthoma81@gmail.com>
2023-02-22 14:19:50 -0600

    Exam 1 (last review day)

--- Exam1Review/src/mainpackage/MainClass.java
+++ Exam1Review/src/mainpackage/MainClass.java
@@ -21,6 +21,9 @@ public class MainClass {
 		char letter; //declare char variable called letter
 		int alignment;
 		
+		double exampleNumber;
+		Double exampleNumber2;
+		
 		//const String shipName = "Darth Vader's Tie Fighter";
 		
 		//assignment operator
@@ -58,6 +61,37 @@ public class MainClass {
 		
 		
 		
+		/*"Know About" notes
+		 * Compiling: converts human readable code to 0's and 1's
+		 * Executing: run something
+		 * Source code: Something you write prior to compilation (java code above)
+		 * Bytecode: what you get when you compile java. Not quite machine code
+		 * Interpeted language: entire source code is "read" by another program and used like instructions
+		 * Java virtual machine: program that interprets bytecode
+		 * keyword: word with special meaning in teh laanguage: (int, string, new, etc)
+		 * operator: a symbol that indicates operations on "operands", (in other words, +, -, *, /, etc)
+		 * Syntax: grammar rules of computer language
+		 * identifier: programmer defined name (variable name, class name, etc)
+		 * algorithm: series of logical steps (step 1: declare variable, step 2, assign value, step 3 multiply by ..... etc)
+		 * Comment: piece of writing in code. Doesn't get compiled. helps explain code
+		 * method: Also known as function, group of statements that can be run with one line
+		 * Body: stuff between the curly brackets {}
+		 * Variable declaration: defining what a programmer made word or identifier means. Determines scope, includes data type, "calls variable into existence"
+		 * data types: the type of data that a variable can hold (more specifically, the way in which the zereos and ones are to be understood)
+		 * primative type: a data type that is not an object under the hood. (int, char, short, long, double, are primative types, String is not)
+		*  Reference type: a data type that is actually an object under the hood
+		*  assignment statement: a statement that usually contains an = and assigns a value to a variable (example: ewoks = 9;
+		*  named constants: variables that don't change. Declared with the "final" keyword.
+		*  Variable Scope: part of the program in which a variable can be "seen" by the code
+		*  String: object that generally represents a combination of letters or words.
+		*  Class: Block of code, thought of as a blueprint, contains 0 or more methods, and 0 or more class fields (variables). Can be instantiated to get objects
+		*  Object: "Instance of a class" can be thought of as a named working copy of a class.
+		*/
+		
+		
+		
+		
+		
 	}
 	
 	public static void printSithName(String name)
@@ -66,5 +100,4 @@ public class MainClass {
 		System.out.println(newName);
 	}
 	
-
 }
